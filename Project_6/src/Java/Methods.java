842d1eef86fa0043f085917ae54b46a92169bbcb
Minhaz01 <33205845+Minhaz01@users.noreply.github.com>
2017-11-07 17:59:46 -0600

    Update Methods.java
    hi

--- Project_6/src/Java/Methods.java
+++ Project_6/src/Java/Methods.java
@@ -4,6 +4,8 @@ public class Methods {
 
 	public static void main(String[] args) {
 		System.out.println(" This is the 4th trying for GIT");
+		System.out.println(" This is the 5th trying for GIT");
+		System.out.println(" This is the 6th trying for GIT");
 
 	}
 
